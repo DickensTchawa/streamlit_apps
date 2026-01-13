2a37b716a2d58da4d5ae53a950221af337a69dfc
DickensTchawa <114138803+DickensTchawa@users.noreply.github.com>
2026-01-13 11:20:11 +0100

    update of geographic app

--- geographic.py
+++ geographic.py
@@ -34,4 +34,3 @@ if plot:
         fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude',
         size=primary, color=secondary, zoom=4, size_max=35, mapbox_style='open-street-map',
         width=1200, height=700, hover_name='District')
-        st.plotly_chart(fig,use_container_width=True)
